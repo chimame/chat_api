052312fae1f955a7412bdb5b78fec6b910944cb1
chimame <rito.tamata@gmail.com>
2017-04-14 23:23:13 +0900

    scenario 29

--- client/app/components/Message/index.js
+++ client/app/components/Message/index.js
@@ -1,9 +1,14 @@
 import React, { PropTypes } from 'react'
 
 const Message = ({actions, id, text, onClick}) => {
+  const handleDelete = () => {
+    actions.deleteMessage(id)
+  }
+
   return (
     <div>
       <span onClick={onClick}>{text}</span>
+      <input type="button" value='削除' onClick={handleDelete} />
     </div>
   )
 }
